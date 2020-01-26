cec80f9184c4f3f2a87671c1f0268b970c01a18f
André Sebastian-Bath <a.sebastian@posteo.de>
2020-01-26 19:20:22 +0100

    connect button with led logic

--- buttonandled.py
+++ buttonandled.py
@@ -3,3 +3,19 @@ import time
 
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(22, GPIO.OUT)
+GPIO.setup(23, GPIO.IN)
+
+while True:
+    if GPIO.input(23) == 0:
+        #Ausschalten
+        GPIO.output(22, GPIO.HIGH)
+        
+    else:
+        #Einschalten
+        GPIO.output(22, GPIO.LOW)
+        
+#for i in range(5):
+ #   GPIO.output(22, GPIO.HIGH)
+  #  time.sleep(0.5)
+   # GPIO.output(22, GPIO.LOW)
+    #time.sleep(0.5)
